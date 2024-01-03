5ec58bc2bc163a47278e194696493fec04534f8f
elmehdi elfatihi <elmehdielfatihi1111@gmail.com>
2024-01-03 17:39:45 +0100

    move from gridsome to gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio-optimized`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.svg',
+      },
+    }]
+};
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1704297051607
