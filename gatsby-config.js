165fb57e378b4b12e4df674fe39fba926e61892c
El Mehdi El fatihi <elfatihidev@gmail.com>
2025-04-11 13:58:49 +0100

    test ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,24 @@ module.exports = {
         icon: 'src/images/icon.svg',
       },
     },
-    `gatsby-plugin-netlify`
+    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-W5N1D8M789", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
   ]
 };
